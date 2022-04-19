5904e862df1763932530eebc495b53fb14c49a7c
IshVK <103988727+IshVK@users.noreply.github.com>
2022-04-19 13:22:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,4 +6,4 @@ streamlit.header('My Mom cooks delicious meals')
 
 
 streamlit.text('The Breakfast menu is so simple but the lunch has more varities') 
-               streamlit.text('and the dinner is soooo good')
+streamlit.text('and the dinner is soooo good')
