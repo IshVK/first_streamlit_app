ed86784c08f244db65c639e82b7e0bb665fa0a9e
IshVK <103988727+IshVK@users.noreply.github.com>
2022-04-19 13:21:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My Parents doesnt own a healthy diner')
 
-streamlit.title('My Mom cooks delicious meals')
+streamlit.header('My Mom cooks delicious meals')
 
 
-streamlit.title('The Breakfast menu is so simple but the lunch has more varities and dinner is soooo good')
+streamlit.text('The Breakfast menu is so simple but the lunch has more varities and dinner is soooo good')
